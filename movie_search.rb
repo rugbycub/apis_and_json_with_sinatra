5468fb350b0e89543d636dde330da92565707a60
Charlie Nuttall <CENuttall@mac.com>
2014-01-17 17:59:07 -0800

    "fixed issues with linking to search results page"

--- movie_search.rb
+++ movie_search.rb
@@ -3,27 +3,27 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
-MY_DB = []
-
-
 get "/" do
-	@movies = MY_DB
-
 	erb :index
 end
 
-post '/' do
-	movie = params[:movie]
-	MY_DB << movie
-	request = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => movie[title] })
-	parsed_results = JSON.parse( request.response_body )
-	movies = parsed_results["Search"]
-    sorted = movies.sort_by { |hash| hash["Year"] }
 
-    display = ""
-    sorted.each do |movie|
-      @movies << {:title => movie["Title"], :year => movie["Year"], :imdb => movie["imdbID"]}   
-		end
+post '/search_result' do
+    search_str = params[:movie]
+
+    request = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => search_str })
+    parsed_results = JSON.parse( request.response_body )
+    @movies = parsed_results["Search"]
     erb :search_results
+    #@sorted = movies.sort_by { |hash| hash["Year"] }
+end
+
+get '/movie_information/:imdbID' do
+    id = params[:imdbID].to_s
+
+    request = Typhoeus.get("http://www.omdbapi.com/", :params => {:i => id })
+    @results = JSON.parse( request.response_body )
+    erb :movie_info
+
 end
 
